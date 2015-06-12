0128f554bc13013fcfa067df9930e9138207dbab
Toxicat <guilma_p@epitech.eu>
2015-06-13 00:44:13 +0200

    Add sound when turning on

--- miner-app/controllers/RootCtrl.js
+++ miner-app/controllers/RootCtrl.js
@@ -1,3 +1,9 @@
 MinerControllers.controller('RootCtrl', ['$scope', function ($scope) {
 	$scope.version = "0.0.1";
+
+	$scope.switchSound = function() {
+		var audio = document.getElementById('switchSound');
+		audio.volume = 0.5;
+		audio.play();
+	}
 }]);
