346e644af9f3c372ec68a4b85704b6cf07335e7f
Toxicat <guilma_p@epitech.eu>
2015-06-14 20:33:22 +0200

    Registration work (without status message)

--- miner-app/controllers/RegisterCtrl.js
+++ miner-app/controllers/RegisterCtrl.js
@@ -1,3 +1,21 @@
-MinerControllers.controller('RegisterCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
-	
+MinerControllers.controller('RegisterCtrl', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
+	$scope.newUser = {
+		username: "",
+		password: "",
+		email: ""
+	};
+	$scope.status = 0;
+	$scope.response = "";
+
+	$scope.submitRegister = function () {
+		if ($scope.newUser.username != "" && $scope.newUser.password != "" && $scope.newUser.email != "") {
+			var promise = $http.post('/signup', $scope.newUser);
+
+			promise.then(function (resolve) {
+				console.log(resolve);
+			}, function (reject) {
+				console.log(reject);
+			});
+		}
+	}
 }]);
