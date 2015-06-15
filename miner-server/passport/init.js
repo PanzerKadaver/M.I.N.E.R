18cb61b10711ef61c60be8e95bc7af025a98808a
Toxicat <guilma_p@epitech.eu>
2015-06-15 21:11:19 +0200

    Remove logout

--- miner-server/passport/init.js
+++ miner-server/passport/init.js
@@ -1,10 +1,9 @@
 var login = require('./login');
 var signup = require('./signup');
-var logout = require('./logout');
 
 var Convict = require('../models/Convict');
 
-module.exports= function (passport) {
+module.exports = function (passport) {
 
 	passport.serializeUser(function (convict, done) {
 		console.log('serializing user : ' + convict);
@@ -14,11 +13,10 @@ module.exports= function (passport) {
 	passport.deserializeUser(function (id, done) {
 		Convict.findById(id, function (err, convict) {
 			console.log('deserializing user : ' + convict);
-			done(err, user);
+			done(err, convict);
 		});
 	});
 
 	login(passport);
 	signup(passport);
-	//logout(passport);
 };
