2cd225c6b341df01d72602ca28c6c09b3ce7a197
Toxicat <guilma_p@epitech.eu>
2015-06-15 21:12:18 +0200

    Bettar state handling

--- miner-app/controllers/RootCtrl.js
+++ miner-app/controllers/RootCtrl.js
@@ -1,9 +1,19 @@
-MinerControllers.controller('RootCtrl', ['$scope', function ($scope) {
+MinerControllers.controller('RootCtrl', ['$rootScope', '$scope', '$state', function ($rootScope, $scope, $state) {
 	$scope.version = "0.0.4";
+	$scope.ghostSwitch = false;
+	$scope.currentUser = {};
 
 	$scope.switchSound = function() {
 		var audio = document.getElementById('switchSound');
 		audio.volume = 0.5;
 		audio.play();
 	}
+
+	$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+		//console.log(event, toState, toParams, fromState, fromParams, $scope.ghostSwitch);
+		if (toState.name != "start" && $scope.ghostSwitch == false) {
+			event.preventDefault();
+			$state.go('start');
+		}
+	});
 }]);
